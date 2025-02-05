1cca06801e22dbdb609d926f39500279c36c1642
Starboy <samskywalker001@gmail.com>
2025-02-05 14:31:09 +0000

    toBeOrNotToBe

--- Exercises/JS-30/3-itsOkToBeNotOk/main.ts
+++ Exercises/JS-30/3-itsOkToBeNotOk/main.ts
@@ -1,8 +1,36 @@
-export function add(a: number, b: number): number {
-  return a + b;
+type ToBeOrNotToBe = {
+  toBe: (val: any) => boolean;
+  notToBe: (val: any) => boolean;
+};
+
+export function expect(val: any): ToBeOrNotToBe {
+  return {
+    toBe: (toBeVal: any) => {
+      if (val === toBeVal) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: (notToBeVal: any) => {
+      if (val !== notToBeVal) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log(expect(3).toBe(3));
+  // console.log(expect(3).toBe(6));
+  console.log(expect(6).notToBe(9));
+  // console.log(expect(6).notToBe(6));
 }
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
