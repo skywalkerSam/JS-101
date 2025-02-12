b597a6a501c141128ce78aa8ee83af274d8075a0
Starboy <samskywalker001@gmail.com>
2025-02-12 22:43:56 +0530

    Counter II

--- Exercises/JS-30/4-Counter-II/main.ts
+++ Exercises/JS-30/4-Counter-II/main.ts
@@ -1,21 +1,36 @@
 type Counter = {
-  increment: () => number,
-  decrement: () => number,
-  reset: () => number,
-}
-
-function createCounter(init: number): Counter {
-  //
+  increment: () => number;
+  decrement: () => number;
+  reset: () => number;
 };
 
+export function createCounter(init: number): Counter {
+  let localInitValue: number = init;
+  return {
+    increment: () => {
+      return ++localInitValue;
+    },
+    decrement: () => {
+      return --localInitValue;
+    },
+    reset: () => {
+      localInitValue = init;
+      return localInitValue;
+    },
+  };
+}
+
 /**
-* const counter = createCounter(5)
-* counter.increment(); // 6
-* counter.reset(); // 5
-* counter.decrement(); // 4
-*/
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  // 
+  const counter = createCounter(5);
+  console.log(counter.increment()); // 6
+  console.log(counter.reset()); // 5
+  console.log(counter.decrement()); // 4
 }
