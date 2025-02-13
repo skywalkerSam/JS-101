afa5561299b8bbcad30837267265bc86f39f4782
Starboy <samskywalker001@gmail.com>
2025-02-13 16:08:54 +0530

    initials

---
+++ Exercises/JS-30/6-filterElementsFromArray/main.ts
@@ -0,0 +1,10 @@
+type Fn = (n: number, i: number) => any;
+
+function filter(arr: number[], fn: Fn): number[] {
+  //
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  //
+}
