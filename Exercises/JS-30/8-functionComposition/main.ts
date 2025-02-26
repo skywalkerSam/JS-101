e9a6aa81a3538de0dda8212da2691df4eebe58e3
Starboy <samskywalker001@gmail.com>
2025-02-26 02:42:19 +0000

    f(m(l(x)))

--- Exercises/JS-30/8-functionComposition/main.ts
+++ Exercises/JS-30/8-functionComposition/main.ts
@@ -1,17 +1,25 @@
 type F = (x: number) => number;
 
 export function compose(functions: F[]): F {
-  return function (x) {};
+  return function (x) {
+    if (functions.length === 0) {
+      return x;
+    }
+    for (let i: number = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+    return x;
+  };
 }
 
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
+const fn = compose([(x) => x + 1, (x) => 2 * x]);
+// fn(4); // 9
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("");
+  console.log(fn(4));
+  const box = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
+  console.log(box(3));
 }
 
 // https://leetcode.com/problems/function-composition/?envType=study-plan-v2&envId=30-days-of-javascript
