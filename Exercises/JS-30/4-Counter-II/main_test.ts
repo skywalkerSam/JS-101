1e2a45080f371c29985f68a688b3e5a6d7f8a833
Starboy <samskywalker001@gmail.com>
2025-03-11 06:50:13 +0000

    counter()

--- Exercises/JS-30/4-Counter-II/main_test.ts
+++ Exercises/JS-30/4-Counter-II/main_test.ts
@@ -1,6 +1,13 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { createCounter } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function counterTest() {
+  const counter = createCounter(3);
+  assertEquals(counter.increment(), 4);
+  assertEquals(counter.increment(), 5);
+  assertEquals(counter.increment(), 6);
+  assertEquals(counter.reset(), 3);
+  assertEquals(counter.decrement(), 2);
+  assertEquals(counter.decrement(), 1);
+  assertEquals(counter.decrement(), 0);
 });
