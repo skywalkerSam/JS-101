c83b519650b0c8bda5110f243e777eb4129464ba
Starboy <samskywalker001@gmail.com>
2025-03-12 12:31:33 +0000

    filter()

--- Exercises/JS-30/6-filterElementsFromArray/main.ts
+++ Exercises/JS-30/6-filterElementsFromArray/main.ts
@@ -1,11 +1,4 @@
-export function greaterThan3(num: number): boolean {
-  if (num >= 3) {
-    return true;
-  }
-  return false;
-}
-
-export const anArray: number[] = [0, 1, 3, 6, 9, 12, 15, 18, 21];
+// Custom `.filter()`
 
 // @ts-ignore intentional type any.)
 type Fn = (n: number, i: number) => any;
@@ -23,14 +16,15 @@ export function filter(arr: number[], fn: Fn): number[] {
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(filter(anArray, greaterThan3));
 
-  //.filter()
-  // console.log(anArray.filter((i) => i > 6));
+// if (import.meta.main) {
+//   console.log(filter(anArray, greaterThan3));
 
-  //.map()
-  // console.log(anArray.map((i) => i ** 2));
-}
+//   //.filter()
+//   // console.log(anArray.filter((i) => i > 6));
+
+//   //.map()
+//   // console.log(anArray.map((i) => i ** 2));
+// }
 
 // https://leetcode.com/problems/filter-elements-from-array/description/?envType=study-plan-v2&envId=30-days-of-javascript
--- Exercises/JS-30/6-filterElementsFromArray/main_test.ts
+++ Exercises/JS-30/6-filterElementsFromArray/main_test.ts
@@ -1,6 +1,27 @@
 import { assertEquals } from "@std/assert";
-import { filter, anArray, greaterThan3 } from "./main.ts";
+import { filter } from "./main.ts";
+
+const anArray: number[] = [0, 1, 3, 6, 9, 12, 15, 18, 21];
+
+function greaterThan3(num: number): boolean {
+  if (num >= 3) {
+    return true;
+  }
+  return false;
+}
+
+function greaterThan15(num: number): boolean {
+  if (num > 15) {
+    return true;
+  }
+  return false;
+}
 
 Deno.test(function filterTest() {
   assertEquals(filter(anArray, greaterThan3), [3, 6, 9, 12, 15, 18, 21]);
+});
+
+// #2
+Deno.test(function secondFilterTest() {
+  assertEquals(filter(anArray, greaterThan15), [18, 21]);
 });
