4d5f7bb654a4083bd5d68b6bba9f4f561676ea68
Starboy <samskywalker001@gmail.com>
2025-03-12 11:25:17 +0000

    reduce()

--- Exercises/JS-30/7-arrayReduceTransformation/main_test.ts
+++ Exercises/JS-30/7-arrayReduceTransformation/main_test.ts
@@ -3,5 +3,9 @@ import { someArr, sumFn, reduce, multipleFn } from "./main.ts";
 
 Deno.test(function reduceTest() {
   assertEquals(reduce(someArr, sumFn, 0), 18);
+});
+
+// #2
+Deno.test(function secondReduceTest() {
   assertEquals(reduce(someArr, multipleFn, 1), 162);
 });
