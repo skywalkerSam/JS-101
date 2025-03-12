3e9f584f7323a634cf4a12a48a8a47292b6126b3
Starboy <samskywalker001@gmail.com>
2025-03-12 19:00:31 +0000

    oneFunctionCallWrapper

--- Exercises/JS-30/10-allowOneFunctionCall/main.ts
+++ Exercises/JS-30/10-allowOneFunctionCall/main.ts
@@ -1,8 +1,29 @@
-export function add(a: number, b: number): number {
-  return a + b;
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+// @ts-ignore Intentional broad fn() definition.)
+export function once(fn: Function): OnceFn {
+  let called: boolean = false;
+  return function (...args) {
+    if (!called) {
+      called = true;
+      return fn(...args);
+    } else {
+      return undefined;
+    }
+  };
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
--- Exercises/JS-30/10-allowOneFunctionCall/main_test.ts
+++ Exercises/JS-30/10-allowOneFunctionCall/main_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { once } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const someFn = (x: number, y: number, z: number) => {
+  return x + y + z;
+};
+
+Deno.test(function oneFunctionCallTest() {
+  const box = once(someFn);
+  assertEquals(box(3, 6, 9), 18);
+  assertEquals(box(3, 6, 9), undefined);
 });
