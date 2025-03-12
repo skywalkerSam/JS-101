f348d7e38d3db6054f9f05ff80bb23c5b23d332a
Starboy <samskywalker001@gmail.com>
2025-03-12 11:49:59 +0000

    increaseByOne()

--- Exercises/JS-30/2-Counter/main.ts
+++ Exercises/JS-30/2-Counter/main.ts
@@ -1,23 +1,25 @@
+// Simple Counter
+
+// You'll see closure in action here!
 export function createCounter(n: number): () => number {
-  // you'll see closure in action here, always use this pattern for f()s.
   return function () {
-    return n++;   // use "++n" if you don't wanna skip the current value.) 
+    return n++; // use "++n" if you don't wanna skip the current value.)
   };
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  // you can't "n++" using this pattern.
-  // console.log(createCounter(9)());
-  // console.log(createCounter(9)());
-  // console.log(createCounter(9)());
+// if (import.meta.main) {
+//   // you can't "n++" using this pattern.
+//   // console.log(createCounter(9)());
+//   // console.log(createCounter(9)());
+//   // console.log(createCounter(9)());
 
-  const increaseByOne = createCounter(3);
-  console.log(increaseByOne());   // because, you're using "n++", and not "++n".)
-  console.log(increaseByOne());
-  console.log(increaseByOne());
-  console.log(increaseByOne());
-}
+//   const increaseByOne = createCounter(3);
+//   console.log(increaseByOne());   // because, you're using "n++", and not "++n".)
+//   console.log(increaseByOne());
+//   console.log(increaseByOne());
+//   console.log(increaseByOne());
+// }
 
 /** This tells ya how they're running thy function. Cool.)
  * const counter = createCounter(10)
@@ -26,4 +28,4 @@ if (import.meta.main) {
  * counter() // 12
  */
 
-// NOTE: I'll write the tests some other day.) - Today's the day.) 
+// NOTE: I'll write the tests some other day.) - Today's the day.)
