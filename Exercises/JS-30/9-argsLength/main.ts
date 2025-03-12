8a2b1059cc951a2b5836f0d068ae0ba75a39c352
Starboy <samskywalker001@gmail.com>
2025-03-12 12:38:05 +0000

    args.length

--- Exercises/JS-30/9-argsLength/main.ts
+++ Exercises/JS-30/9-argsLength/main.ts
@@ -15,6 +15,7 @@ export function argumentsLength(...args: JSONValue[]): number {
  */
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(argumentsLength(1, 2, 3, "idk", 0));
-}
+
+// if (import.meta.main) {
+//   console.log(argumentsLength(1, 2, 3, "idk", 0));
+// }
--- Exercises/JS-30/9-argsLength/main_test.ts
+++ Exercises/JS-30/9-argsLength/main_test.ts
@@ -5,10 +5,3 @@ Deno.test(function argumentsLengthTest() {
   assertEquals(argumentsLength(1, 2), 2);
   assertEquals(argumentsLength(3, 6, 9), 3);
 });
-
-// import { assertEquals } from "@std/assert";
-// import { add } from "./main.ts";
-
-// Deno.test(function addTest() {
-//   assertEquals(add(2, 3), 5);
-// });
