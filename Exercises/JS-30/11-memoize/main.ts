fda409933162248ff0f4dec3433869da33e62570
Starboy <samskywalker001@gmail.com>
2025-03-30 18:43:04 +0000

    memo()

--- Exercises/JS-30/11-memoize/main.ts
+++ Exercises/JS-30/11-memoize/main.ts
@@ -6,11 +6,17 @@ type Fn = (...params: number[]) => number;
  * @returns A memoized version of the given function
  * @see https://leetcode.com/problems/memoize/description/?envType=study-plan-v2&envId=30-days-of-javascript
  */
-export function memoize(fn: Fn): Fn {
-  let memoizedArgs: number[] = [];
+export function memo(fn: Fn): Fn {
+  let memoizedArgs: string = "";
+  let cachedValue: number = 1;
   return function (...args) {
-    for (let i: number = 0; i < args.length; i++) {
-      memoizedArgs.push(args[i]);
+    if (JSON.stringify(args) === memoizedArgs) {
+      return cachedValue;
+    } else {
+      const memoizedValue = fn(...args);
+      cachedValue = memoizedValue;
+      memoizedArgs = JSON.stringify(args);
+      return memoizedValue;
     }
   };
 }
@@ -24,4 +30,21 @@ export function memoize(fn: Fn): Fn {
  * memoizedFn(2, 3) // 5
  * memoizedFn(2, 3) // 5
  * console.log(callCount) // 1
+ */
+
+/** Notes/Resources
+ * 
+ * w/ JSON.stringify() bc, JavaScript is JavaScript.)
+ *
+ * let one = JSON.stringify([5, 6])
+ * let another = JSON.stringify([5, 6])
+ * console.log(one === another) //true
+ *
+ *
+ * w/o JSON.stringify()
+ *
+ * let one = [3, 6]
+ * let another = [3, 6]
+ * console.log(one === another) //false
+ *
  */
--- Exercises/JS-30/11-memoize/main_test.ts
+++ Exercises/JS-30/11-memoize/main_test.ts
@@ -1,6 +1,39 @@
 import { assertEquals } from "@std/assert";
-import { memoize } from "./main.ts";
+import { memo } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(memoize(2, 3), 5);
+Deno.test(function memoTest() {
+  let callCount = 0;
+  const memoFn = memo(function (a, b) {
+    callCount += 1;
+    return a + b;
+  });
+  assertEquals(memoFn(3, 6), 9);
+  assertEquals(memoFn(3, 6), 9);
+  assertEquals(callCount, 1);
+
+  // console.log(callCount); // 1
+});
+
+Deno.test(function memoTestTheSecond() {
+  let callCount = 0;
+  const memoFn = memo(function (a, b) {
+    callCount += 1;
+    return a + b;
+  });
+  assertEquals(memoFn(3, 6), 9);
+  assertEquals(memoFn(6, 3), 9);
+  assertEquals(callCount, 2);
+
+});
+
+Deno.test(function freshMemoInsertionTest() {
+  let callCount = 0;
+  const memoFn = memo(function (a, b) {
+    callCount += 1;
+    return a + b;
+  });
+  assertEquals(memoFn(3, 6), 9);
+  assertEquals(memoFn(6, 9), 15);
+  assertEquals(memoFn(3, 3), 6);
+  assertEquals(callCount, 3);
 });
