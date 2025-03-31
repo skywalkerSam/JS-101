dd399a8555c6069823b79c4a215a380fbb33f8c2
Starboy <samskywalker001@gmail.com>
2025-03-31 23:32:32 +0000

    📐 Pythagorean Triplets

--- Exercises/Pythagorean-Triplets/main.ts
+++ Exercises/Pythagorean-Triplets/main.ts
@@ -1,16 +1,32 @@
-export default function perfectHypotenuse() {
+/**
+ * Calculates the hypotenuse of a right triangle given the lengths of the other two sides.
+ *
+ * @param {number} a - The length of one side of the triangle.
+ * @param {number} b - The length of the other side of the triangle.
+ * @returns {number} The length of the hypotenuse.
+ */
+
+export function findHypotenuse(a: number, b: number) {
+  const hypotenuse = Math.sqrt(a ** 2 + b ** 2);
+  return hypotenuse;
+}
+
+/**
+ * Calculates the first ten Pythagorean triplets.
+ *
+ * @returns {number[]} The first ten Pythagorean triplets.
+ *
+ * @todo
+ * What exactly do we do here...? brute force??
+ *
+ */
+export function pythagoreanTriplets() {
   // smallest values for a 📐 that has a perfect `c` i.e w/o floating values
   let a: number = 3;
   let b: number = 4;
   let c: number = 5;
   let hypotenuseStore: number[] = [];
-  for (let i = 0; hypotenuseStore.length < 10; i++) {
-    // what exactly do we do here...? brute force??
 
-    if (c ** 2 === a ** 2 + b ** 2) {
-      hypotenuseStore.push(c);
-    }
-  }
   console.log(hypotenuseStore);
   return hypotenuseStore;
 }
