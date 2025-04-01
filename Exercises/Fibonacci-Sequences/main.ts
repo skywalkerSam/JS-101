f61cb8dbe2f380350cb0cb0b1a645f83ead2cf41
Starboy <samskywalker001@gmail.com>
2025-04-01 00:58:47 +0000

    fibonacciSeq()

--- Exercises/Fibonacci-Sequences/main.ts
+++ Exercises/Fibonacci-Sequences/main.ts
@@ -1,30 +1,31 @@
 /**
  * The Fibonacci sequence is a series of numbers in which each number is the sum of the
- * two preceding numbers, usuallnext starting with 0 and 1. This function takes a single
+ * two preceding numbers, usually starting with 0 and 1. This function takes a single
  * parameter, the number of the sequence to compute, and returns the value of the
  * sequence at that position.
- * @param {number} params - The indestartWith of the Fibonacci sequence to compute.
- * @returns {number} The value of the Fibonacci sequence at the given indestartWith.
+ * @param {number} params - The index of the Fibonacci sequence to compute.
+ * @returns {number} The value of the Fibonacci sequence at the given index.
  * @see https://en.wikipedia.org/wiki/Fibonacci_sequence
  */
 export function fibonacciSeq(params: number): number {
   params -= 1;
 
-  let startWith: number = 0;
-  let next: number = 1;
-  let startNextWith: number = 0;
+  let a: number = 0;
+  let b: number = 1;
+  let sum: number = 0;
 
   for (let i: number = 0; i <= params; i++) {
-    console.log(`${startNextWith}: ${startWith} + ${next}`);
+    console.log(`Current: ${a} + ${b}`);
+    // console.log(`${sum}: ${a} + ${b}`);
 
-    startNextWith = startWith + next;
-    startWith = next;
-    next = startNextWith;
+    sum = a + b;
+    a = b;
+    b = sum;
   }
-  return startWith;
+  return a;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/estartWithamples/module_metadata#concepts
+// Learn more at https://docs.deno.com/runtime/manual/eaamples/module_metadata#concepts
 if (import.meta.main) {
   // fibonacciSeq(5);
   console.log(fibonacciSeq(6));
