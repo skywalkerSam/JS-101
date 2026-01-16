659760a8cdc33851b5de353755f9bd62632fb8dc
Starboy <samskywalker001@gmail.com>
2026-01-16 02:56:06 +0000

    Array.map()

--- JS-30/04a-Array.map-demo.ts
+++ JS-30/04a-Array.map-demo.ts
@@ -6,21 +6,27 @@
 export function createMap(arr: number[]) {
   return {
     plus: function (plusValue: number) {
-      const transformedArr= arr.map((i) => i + plusValue);
+      const transformedArr = arr.map((i) => i + plusValue);
       return transformedArr;
     },
+
     exponent: (exponentValue: number) => {
-      const transformedArr= arr.map((i) => i ** exponentValue);
+      const transformedArr = arr.map((i) => i ** exponentValue);
       return transformedArr;
     },
+
     square: function () {
-      const transformedArr= arr.map((i) => i ** 2);
+      const transformedArr = arr.map((i) => i ** 2);
       return transformedArr;
     },
 
     cube: function () {
-      const transformedArr= arr.map((i) => i ** 3);
+      const transformedArr = arr.map((i) => i ** 3);
       return transformedArr;
     },
   };
 }
+
+const arr = createMap([1, 2, 3, 4]);
+const cube = arr.cube();
+console.log(cube); // [1, 8, 27, 64]
