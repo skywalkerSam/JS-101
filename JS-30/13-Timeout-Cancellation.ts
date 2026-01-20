86ffa971608444109c47eefca433393c252a9195
Starboy <samskywalker001@gmail.com>
2026-01-20 04:08:12 +0000

    initials

--- JS-30/13-Timeout-Cancellation.ts
+++ JS-30/13-Timeout-Cancellation.ts
@@ -8,18 +8,24 @@
  * Initially, the execution of the function fn should be delayed by t milliseconds.
  *
  * If, before the delay of t milliseconds, the function cancelFn is invoked, it should cancel the delayed execution of fn. Otherwise, if cancelFn is not invoked within the specified delay t, fn should be executed with the provided args as arguments.
+ */
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void;
+
+/**
  *
  * @param fn
  * @param args
  * @param t
  */
-
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type Fn = (...args: JSONValue[]) => void
-
-function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
-
-}
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {}
 
 /**
  *  const result = [];
